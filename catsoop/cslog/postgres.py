c35d35a6b19c6bce7dd1d44fc7f7fa8e63215738
adam j hartz <hz@mit.edu>
2020-05-31 14:28:50 -0400

    copy/paste error

--- catsoop/cslog/postgres.py
+++ catsoop/cslog/postgres.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-Logging mechanisms using the [PostgreSQL](https://www.postgresql.org/)
+Logging mechanisms using [PostgreSQL](https://www.postgresql.org/)
 """
 
 import time
