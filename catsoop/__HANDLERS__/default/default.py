4c7657712b376c86cb763aef5e551345b1f45e74
adam j hartz <hz@mit.edu>
2020-09-10 14:17:54 -0400

    close xss vulnerability in default handler

--- catsoop/__HANDLERS__/default/default.py
+++ catsoop/__HANDLERS__/default/default.py
@@ -16,6 +16,7 @@
 
 import os
 import re
+import html
 import json
 import time
 import uuid
@@ -68,7 +69,7 @@ def _n(n):
 
 
 def _unknown_handler(action):
-    return lambda x: "Unknown Action: %s" % action
+    return lambda x: "Unknown Action: %s" % html.escape(action)
 
 
 def _get(context, key, default, cast=lambda x: x):
