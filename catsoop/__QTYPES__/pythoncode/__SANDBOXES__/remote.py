5516db1a786ce41de943b8b32b3d63212c789354
adam j hartz <hz@mit.edu>
2021-07-19 13:19:12 -0400

    change sandbox location

--- catsoop/__QTYPES__/pythoncode/__SANDBOXES__/remote.py
+++ catsoop/__QTYPES__/pythoncode/__SANDBOXES__/remote.py
@@ -17,7 +17,7 @@
 import json
 import urllib.request, urllib.parse, urllib.error
 
-SANDBOX_URL = "https://catsoop.org/python_sandbox_v2019.9/"
+SANDBOX_URL = "https://catsoop.org/python_sandbox/"
 
 
 def run_code(
