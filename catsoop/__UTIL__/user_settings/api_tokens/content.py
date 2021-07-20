38fb15f5b789e5f6d30f937b5c4e4b271c39c047
adam j hartz <hz@mit.edu>
2021-07-20 18:37:34 -0400

    use .py format for api token management page instead of .catsoop

--- catsoop/__UTIL__/user_settings/api_tokens/content.py
+++ catsoop/__UTIL__/user_settings/api_tokens/content.py
@@ -1,4 +1,3 @@
-<python>
 tokens = sorted(set(csm_api.get_api_tokens(globals(), cs_username)))
 
 prefix_length = 5
@@ -9,7 +8,10 @@ while True:
 
 if prefixes:
     plural = "s" if len(tokens) != 1 else ""
-    print('You currently have <b>%s</b> API token%s in the system.  Information about your token%s is shown below.  You can also <a href="CURRENT/new-token">generate an additional token</a>.' % (len(tokens), plural, plural))
+    print(
+        '<p>You currently have <b>%s</b> API token%s in the system.  Information about your token%s is shown below.  You can also <a href="CURRENT/new-token">generate an additional token</a>.</p>'
+        % (len(tokens), plural, plural)
+    )
     print("<center>")
     print("<table border='1'>")
     print("<tr><th>Token</th><th>Delete?</th></tr>")
@@ -17,9 +19,10 @@ if prefixes:
         print("<tr>")
         print(f"<td><tt>{p}...</tt></td>")
         print(f'<td><a href="CURRENT/delete-token?prefix={p}">Delete</a>')
-    print('</table>')
-    print('</center>')
+    print("</table>")
+    print("</center>")
     print()
 else:
-    print('You currently have no API tokens in the system.  You can <a href="CURRENT/new-token">generate a new token</a>.')
-</python>
+    print(
+        '<p>You currently have no API tokens in the system.  You can <a href="CURRENT/new-token">generate a new token</a>.</p>'
+    )
