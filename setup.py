492ff6602e25d64319f91b74ba89cb9c3b956488
adam j hartz <hz@mit.edu>
2022-08-16 09:16:42 -0400

    python 3.10

--- setup.py
+++ setup.py
@@ -163,6 +163,7 @@ def main():
                 "Programming Language :: Python :: 3.7",
                 "Programming Language :: Python :: 3.8",
                 "Programming Language :: Python :: 3.9",
+                "Programming Language :: Python :: 3.10",
                 "Topic :: Education",
                 "Topic :: Internet :: WWW/HTTP :: WSGI",
             ],
