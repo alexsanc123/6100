d0a5d5fc6fe4b34c84ca7d03d7684bd0b53d7dcc
adam j hartz <hz@mit.edu>
2023-07-13 09:32:53 -0400

    better bullet point management?

--- catsoop/__QTYPES__/multiplechoice/multiplechoice.py
+++ catsoop/__QTYPES__/multiplechoice/multiplechoice.py
@@ -216,8 +216,7 @@ def render_html_radio(last_log, **info):
             _s = " checked"
         else:
             _s = ""
-        if ix != 0:
-            out += '<br style="margin-bottom: 10px;"/>'
+        out += '<div style="margin-bottom; 10px;">'
         out += '<input type="radio" name="%s_opts" id="%s_opts_%s" value="%s"%s />' % (
             name,
             name,
@@ -231,6 +230,7 @@ def render_html_radio(last_log, **info):
             ix,
             text,
         )
+        out += '</div>'
     out += "\n</div>"
     out += '<input type="hidden" name="%s" id="%s" value="%s">' % (
         name,
